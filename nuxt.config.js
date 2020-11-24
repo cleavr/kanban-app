d19370d5920efacecbef5c21122101d7ab0091c3
Adam Miedema <armgitaar@yahoo.com>
2020-11-24 14:16:49 -0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: process.env.TITLE,
+    title: process.env.TITLE || 'No Title',
     meta: [
       { charset: 'utf-8' },
       {
