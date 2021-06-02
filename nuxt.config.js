d5fd0eee54e0daec4102653d5b334a699bf93c64
Adam Miedema <armgitaar@yahoo.com>
2021-06-02 13:36:15 -0600

    pwa teaks

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
+  // ssr: false,
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
@@ -58,6 +58,16 @@ export default {
       },
     ],
   ],
+  manifest: {
+    name: 'PWA test',
+    short_name: 'Nuxt.js PWA',
+    lang: 'en',
+    display: 'standalone',
+  },
+
+  pwa: {
+    icon: false // disables the icon module
+  },
 
   toast: {
     position: 'bottom-center',
